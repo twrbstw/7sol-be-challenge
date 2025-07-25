2360890c6c5cafe2cfa447e7fc0518dfd11bebd3
twrbstw <sam.b19250@gmail.com>
2025-07-26 04:21:51 +0700

    update unit test

--- internal/app/services/user_test.go
+++ internal/app/services/user_test.go
@@ -7,6 +7,7 @@ import (
 	mockPorts "seven-solutions-challenge/internal/app/ports/mocks"
 	"seven-solutions-challenge/internal/app/services"
 	"seven-solutions-challenge/internal/domain"
+	e "seven-solutions-challenge/pkg/errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -116,5 +117,166 @@ func TestListError(t *testing.T) {
 	resp, err := userService.List(ctx)
 
 	assert.Nil(t, resp)
+	assert.Equal(t, expected, err)
+}
+
+func TestCreateSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
+	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
+	userService := services.NewUserService(mockUserRepo, mockBcryptHasher)
+
+	req := requests.CreateUserReq{
+		Name:     "test_name",
+		Email:    "test_email",
+		Password: "test_password",
+	}
+
+	hashedPassword := "test_hashed_password"
+	expected := responses.CreateUserResp{
+		Id:        "test_id",
+		Name:      req.Name,
+		Email:     req.Email,
+		CreatedAt: timeNow,
+	}
+
+	mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return(hashedPassword, nil)
+	mockUserRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(&domain.User{
+		Id:        "test_id",
+		Name:      req.Name,
+		Email:     req.Email,
+		Password:  hashedPassword,
+		CreatedAt: timeNow,
+	}, nil)
+
+	resp, err := userService.Create(ctx, req)
+
+	assert.Nil(t, err)
+	assert.Equal(t, &expected, resp)
+}
+
+func TestCreateBcryptHasherError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
+	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
+	userService := services.NewUserService(mockUserRepo, mockBcryptHasher)
+
+	req := requests.CreateUserReq{
+		Name:     "test_name",
+		Email:    "test_email",
+		Password: "test_password",
+	}
+
+	expected := errors.New(e.ERR_SERVICE_HASHING)
+
+	mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return("", errors.New(e.ERR_SERVICE_HASHING))
+
+	resp, err := userService.Create(ctx, req)
+
+	assert.Nil(t, resp)
+	assert.Equal(t, expected, err)
+}
+
+func TestCreateUserRepoError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
+	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
+	userService := services.NewUserService(mockUserRepo, mockBcryptHasher)
+
+	req := requests.CreateUserReq{
+		Name:     "test_name",
+		Email:    "test_email",
+		Password: "test_password",
+	}
+
+	hashedPassword := "test_hashed_password"
+	expected := errors.New("test_error")
+
+	mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return(hashedPassword, nil)
+	mockUserRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil, errors.New("test_error"))
+
+	resp, err := userService.Create(ctx, req)
+
+	assert.Nil(t, resp)
+	assert.Equal(t, expected, err)
+}
+
+func TestUpdateSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
+	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
+	userService := services.NewUserService(mockUserRepo, mockBcryptHasher)
+
+	mockUserRepo.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+
+	err := userService.Update(ctx, requests.UpdateUserReq{
+		Id:    "test_id",
+		Name:  "test_name",
+		Email: "test_email",
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestUpdateError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
+	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
+	userService := services.NewUserService(mockUserRepo, mockBcryptHasher)
+
+	expected := errors.New("test_error")
+	mockUserRepo.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.New("test_error"))
+
+	err := userService.Update(ctx, requests.UpdateUserReq{
+		Id:    "test_id",
+		Name:  "test_name",
+		Email: "test_email",
+	})
+
+	assert.Equal(t, expected, err)
+}
+
+func TestDeleteSuccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
+	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
+	userService := services.NewUserService(mockUserRepo, mockBcryptHasher)
+
+	mockUserRepo.EXPECT().Delete(gomock.Any(), gomock.Any()).Return(nil)
+
+	err := userService.Delete(ctx, requests.DeleteUserReq{
+		Id: "test_id",
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestDeleteError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
+	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
+	userService := services.NewUserService(mockUserRepo, mockBcryptHasher)
+
+	expected := errors.New("test_error")
+	mockUserRepo.EXPECT().Delete(gomock.Any(), gomock.Any()).Return(errors.New("test_error"))
+
+	err := userService.Delete(ctx, requests.DeleteUserReq{
+		Id: "test_id",
+	})
+
 	assert.Equal(t, expected, err)
 }
