13c9338cc8ad25eb07c1c3c5e835730e1e7620b3
twrbstw <sam.b19250@gmail.com>
2025-07-30 00:19:34 +0700

    refactor auth unit test

--- internal/app/services/auth_test.go
+++ internal/app/services/auth_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"seven-solutions-challenge/internal/adapters/inbound/http/requests"
 	"seven-solutions-challenge/internal/adapters/inbound/http/responses"
+	"seven-solutions-challenge/internal/app/ports"
 	mockPorts "seven-solutions-challenge/internal/app/ports/mocks"
 	"seven-solutions-challenge/internal/app/services"
 	"seven-solutions-challenge/internal/domain"
@@ -19,182 +20,160 @@ import (
 var ctx = context.Background()
 var timeNow = time.Now()
 
-func TestRegisterSuccess(t *testing.T) {
+func setup(t *testing.T) (ports.IAuthService,
+	*mockPorts.MockIUserRepo,
+	*mockPorts.MockIHasher,
+	*mockPorts.MockIJwtGenerator,
+) {
 	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	t.Cleanup(ctrl.Finish)
 
 	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
-	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
-	mockJwtGenerator := mockPorts.NewMockIJwtGenerator(ctrl)
-	authService := services.NewAuthService(mockUserRepo, mockBcryptHasher, mockJwtGenerator)
+	mockHasher := mockPorts.NewMockIHasher(ctrl)
+	mockJwtGen := mockPorts.NewMockIJwtGenerator(ctrl)
+	authService := services.NewAuthService(mockUserRepo, mockHasher, mockJwtGen)
 
-	mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return("test_hashed_password", nil)
-	output := domain.User{
-		Id:        "test_id",
-		Name:      "test_name",
-		Email:     "test_email",
-		Password:  "test_password",
-		CreatedAt: timeNow,
-	}
-	mockUserRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(&output, nil)
-
-	err := authService.Register(ctx, requests.AuthRegisterReq{
-		Name:     "test_name",
-		Email:    "test_email",
-		Password: "test_password",
-	})
-
-	assert.NoError(t, err)
+	return authService, mockUserRepo, mockHasher, mockJwtGen
 }
 
-func TestRegisterCreateError(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+func TestRegister(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		authService, mockUserRepo, mockBcryptHasher, _ := setup(t)
 
-	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
-	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
-	mockJwtGenerator := mockPorts.NewMockIJwtGenerator(ctrl)
-	authService := services.NewAuthService(mockUserRepo, mockBcryptHasher, mockJwtGenerator)
+		mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return("test_hashed_password", nil)
+		output := domain.User{
+			Id:        "test_id",
+			Name:      "test_name",
+			Email:     "test_email",
+			Password:  "test_password",
+			CreatedAt: timeNow,
+		}
+		mockUserRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(&output, nil)
 
-	mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return("test_hashed_password", nil)
-	expected := errors.New("test_error")
-	mockUserRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil, errors.New("test_error"))
+		err := authService.Register(ctx, requests.AuthRegisterReq{
+			Name:     "test_name",
+			Email:    "test_email",
+			Password: "test_password",
+		})
 
-	err := authService.Register(ctx, requests.AuthRegisterReq{
-		Name:     "test_name",
-		Email:    "test_email",
-		Password: "test_password",
+		assert.NoError(t, err)
 	})
 
-	assert.Equal(t, expected, err)
+	t.Run("create error", func(t *testing.T) {
+		authService, mockUserRepo, mockBcryptHasher, _ := setup(t)
+
+		mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return("test_hashed_password", nil)
+		expected := errors.New("test_error")
+		mockUserRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil, errors.New("test_error"))
+
+		err := authService.Register(ctx, requests.AuthRegisterReq{
+			Name:     "test_name",
+			Email:    "test_email",
+			Password: "test_password",
+		})
+
+		assert.Equal(t, expected, err)
+	})
+
+	t.Run("hash error", func(t *testing.T) {
+		authService, _, mockBcryptHasher, _ := setup(t)
+
+		expected := errors.New(e.ERR_SERVICE_HASHING)
+		mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return("", errors.New(e.ERR_SERVICE_HASHING))
+
+		err := authService.Register(ctx, requests.AuthRegisterReq{
+			Name:     "test_name",
+			Email:    "test_email",
+			Password: "test_password",
+		})
+
+		assert.Equal(t, expected, err)
+	})
 }
 
-func TestRegisterHasherError(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+func TestLogin(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		authService, mockUserRepo, mockBcryptHasher, mockJwtGenerator := setup(t)
 
-	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
-	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
-	mockJwtGenerator := mockPorts.NewMockIJwtGenerator(ctrl)
-	authService := services.NewAuthService(mockUserRepo, mockBcryptHasher, mockJwtGenerator)
+		output := responses.AuthLoginResp{
+			Email: "test_email",
+			Token: "test_token",
+		}
 
-	expected := errors.New(e.ERR_SERVICE_HASHING)
-	mockBcryptHasher.EXPECT().HashPassword(gomock.Any()).Return("", errors.New(e.ERR_SERVICE_HASHING))
+		mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(&domain.User{
+			Id:       "test_id",
+			Name:     "test_name",
+			Email:    "test_email",
+			Password: "test_hashed_password",
+		}, nil)
+		mockBcryptHasher.EXPECT().ComparePassword(gomock.Any(), gomock.Any()).Return(nil)
+		mockJwtGenerator.EXPECT().GenerateJwt(gomock.Any(), gomock.Any()).Return(output.Token, nil)
 
-	err := authService.Register(ctx, requests.AuthRegisterReq{
-		Name:     "test_name",
-		Email:    "test_email",
-		Password: "test_password",
+		resp, err := authService.Login(ctx, requests.AuthLoginReq{
+			Email:    "test_email",
+			Password: "test_password",
+		})
+
+		assert.NoError(t, err)
+		assert.Equal(t, resp.Email, output.Email)
+		assert.Equal(t, resp.Token, output.Token)
 	})
 
-	assert.Equal(t, expected, err)
-}
+	t.Run("get by email error", func(t *testing.T) {
+		authService, mockUserRepo, _, _ := setup(t)
 
-func TestLoginSuccess(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+		expected := errors.New("test_error")
+		mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(nil, errors.New("test_error"))
 
-	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
-	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
-	mockJwtGenerator := mockPorts.NewMockIJwtGenerator(ctrl)
-	authService := services.NewAuthService(mockUserRepo, mockBcryptHasher, mockJwtGenerator)
+		resp, err := authService.Login(ctx, requests.AuthLoginReq{
+			Email:    "test_email",
+			Password: "test_password",
+		})
 
-	output := responses.AuthLoginResp{
-		Email: "test_email",
-		Token: "test_token",
-	}
-
-	mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(&domain.User{
-		Id:       "test_id",
-		Name:     "test_name",
-		Email:    "test_email",
-		Password: "test_hashed_password",
-	}, nil)
-	mockBcryptHasher.EXPECT().ComparePassword(gomock.Any(), gomock.Any()).Return(nil)
-	mockJwtGenerator.EXPECT().GenerateJwt(gomock.Any(), gomock.Any()).Return(output.Token, nil)
-
-	resp, err := authService.Login(ctx, requests.AuthLoginReq{
-		Email:    "test_email",
-		Password: "test_password",
+		assert.Nil(t, resp)
+		assert.Equal(t, expected, err)
 	})
 
-	assert.NoError(t, err)
-	assert.Equal(t, resp.Email, output.Email)
-	assert.Equal(t, resp.Token, output.Token)
-}
+	t.Run("hash error", func(t *testing.T) {
+		authService, mockUserRepo, mockBcryptHasher, _ := setup(t)
 
-func TestLoginGetByEmailError(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+		mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(&domain.User{
+			Id:       "test_id",
+			Name:     "test_name",
+			Email:    "test_email",
+			Password: "test_hashed_password",
+		}, nil)
+		expected := errors.New(e.ERR_SERVICE_INCORRECT_EMAIL_OR_PASSWORD)
+		mockBcryptHasher.EXPECT().ComparePassword(gomock.Any(), gomock.Any()).Return(errors.New(e.ERR_SERVICE_INCORRECT_EMAIL_OR_PASSWORD))
 
-	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
-	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
-	mockJwtGenerator := mockPorts.NewMockIJwtGenerator(ctrl)
-	authService := services.NewAuthService(mockUserRepo, mockBcryptHasher, mockJwtGenerator)
+		resp, err := authService.Login(ctx, requests.AuthLoginReq{
+			Email:    "test_email",
+			Password: "test_password",
+		})
 
-	expected := errors.New("test_error")
-	mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(nil, errors.New("test_error"))
-
-	resp, err := authService.Login(ctx, requests.AuthLoginReq{
-		Email:    "test_email",
-		Password: "test_password",
+		assert.Nil(t, resp)
+		assert.Equal(t, expected, err)
 	})
 
-	assert.Nil(t, resp)
-	assert.Equal(t, expected, err)
-}
+	t.Run("", func(t *testing.T) {
+		authService, mockUserRepo, mockBcryptHasher, mockJwtGenerator := setup(t)
 
-func TestLoginBcryptHasherError(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+		mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(&domain.User{
+			Id:       "test_id",
+			Name:     "test_name",
+			Email:    "test_email",
+			Password: "test_hashed_password",
+		}, nil)
+		mockBcryptHasher.EXPECT().ComparePassword(gomock.Any(), gomock.Any()).Return(nil)
+		expected := errors.New(e.ERR_SERVICE_GENERATING_JWT_FAILED)
+		mockJwtGenerator.EXPECT().GenerateJwt(gomock.Any(), gomock.Any()).Return("", errors.New(e.ERR_SERVICE_GENERATING_JWT_FAILED))
 
-	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
-	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
-	mockJwtGenerator := mockPorts.NewMockIJwtGenerator(ctrl)
-	authService := services.NewAuthService(mockUserRepo, mockBcryptHasher, mockJwtGenerator)
+		resp, err := authService.Login(ctx, requests.AuthLoginReq{
+			Email:    "test_email",
+			Password: "test_password",
+		})
 
-	mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(&domain.User{
-		Id:       "test_id",
-		Name:     "test_name",
-		Email:    "test_email",
-		Password: "test_hashed_password",
-	}, nil)
-	expected := errors.New(e.ERR_SERVICE_INCORRECT_EMAIL_OR_PASSWORD)
-	mockBcryptHasher.EXPECT().ComparePassword(gomock.Any(), gomock.Any()).Return(errors.New(e.ERR_SERVICE_INCORRECT_EMAIL_OR_PASSWORD))
-
-	resp, err := authService.Login(ctx, requests.AuthLoginReq{
-		Email:    "test_email",
-		Password: "test_password",
+		assert.Nil(t, resp)
+		assert.Equal(t, expected, err)
 	})
-
-	assert.Nil(t, resp)
-	assert.Equal(t, expected, err)
-}
-
-func TestLoginGenerateJwtError(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockUserRepo := mockPorts.NewMockIUserRepo(ctrl)
-	mockBcryptHasher := mockPorts.NewMockIHasher(ctrl)
-	mockJwtGenerator := mockPorts.NewMockIJwtGenerator(ctrl)
-	authService := services.NewAuthService(mockUserRepo, mockBcryptHasher, mockJwtGenerator)
-
-	mockUserRepo.EXPECT().GetByEmail(gomock.Any(), gomock.Any()).Return(&domain.User{
-		Id:       "test_id",
-		Name:     "test_name",
-		Email:    "test_email",
-		Password: "test_hashed_password",
-	}, nil)
-	mockBcryptHasher.EXPECT().ComparePassword(gomock.Any(), gomock.Any()).Return(nil)
-	expected := errors.New(e.ERR_SERVICE_GENERATING_JWT_FAILED)
-	mockJwtGenerator.EXPECT().GenerateJwt(gomock.Any(), gomock.Any()).Return("", errors.New(e.ERR_SERVICE_GENERATING_JWT_FAILED))
-
-	resp, err := authService.Login(ctx, requests.AuthLoginReq{
-		Email:    "test_email",
-		Password: "test_password",
-	})
-
-	assert.Nil(t, resp)
-	assert.Equal(t, expected, err)
 }
